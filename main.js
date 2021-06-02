79eea1d4ccc7873337032c724f18350483e8cd03
aleksanderkoder <neon.113@hotmail.com>
2021-06-02 03:17:39 +0200

    Update 1

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
 let flippedCardsId = []; 
+let wantedNumberOfCards = 12; 
+let possibleValues = ["!","<",">","&&","=","==","!=","<",">","&","*","{}","[]","var","let","div"];
 
+// Draws random cards and populates the game wrapper
+function populateCards() {
+    for(let cardCount = 0; cardCount < wantedNumberOfCards; cardCount++) {
+        let elemCard = document.createElement("div");
+        elemCard.id = cardCount; 
+        let elemSpanFront = document.createElement("span");
+        elemSpanFront.id = "card-front-" + cardCount; 
+        elemSpanFront.innerHTML = "?";
+        elemSpanFront.classList.add("card-front"); 
+        let elemSpanBack = document.createElement("span");
+        elemSpanBack.id = "card-back-" + cardCount;
+        elemSpanBack.classList.add("card-back");
+        elemCard.appendChild(elemSpanFront);
+        elemCard.appendChild(elemSpanBack);
+        document.getElementById("game-wrapper").appendChild(elemCard); 
+    }
+    
 
+}
+
+// Flips selected card
 function turnCard(el) {
     if(flippedCardsId.length < 2 && !el.classList.contains("flipped")) {
         console.log("Turning card..."); 
@@ -15,10 +37,18 @@ function turnCard(el) {
 
 }
 
+// Resets flipped cards to starting state
 function resetCards() {
     console.log("Resetting cards...");
     for(let card of flippedCardsId) {
         document.getElementById(card).classList.toggle("flipped");    
     }
     flippedCardsId = []; 
+}
+
+// Checks if selected pair of cards match
+function checkMatching() {
+    for(let card of flippedCardsId) {
+
+    }
 }
